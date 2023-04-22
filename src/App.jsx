f5a8d80de92d738a303d65dfe53c674115abdbed
Irfan Shakeel <mirfanshakeel84@gmail.com>
2023-04-22 20:50:22 +1200

    Fixed Player for mobile and tablets

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
       </div>
 
       {activeSong?.title && (
-        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
+        <div className="absolute h-28 bottom-5 lg:bottom-0 sm:bottom-5 left-5 right-4 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
           <MusicPlayer />
         </div>
       )}
